7f5a709cde138cb5c7f11448db75300a279cd1c9
DhruvPatel2502 <93792385+DhruvPatel2502@users.noreply.github.com>
2023-02-01 09:57:40 -0500

    Added the snackbar

--- logic.js
+++ logic.js
@@ -2,8 +2,9 @@
 const selectFontFace = document.querySelector('#FontFace');
 const selectTextSpace = document.querySelector('#TextSpacing');
 const generate = document.querySelector("#Generate");
-
+let t= selectTextSpace.value; 
 generate.addEventListener('click', (event) => {
+    
     const result = document.querySelector('#generatedCode');
     if (selectFontFace.value == "Select a font face" && !(selectTextSpace.value == "0")) {
         result.textContent = `javascript:(function(){var d=document,id='phltsbkmklt',el=d.getElementById(id),f=d.querySelectorAll('iframe'),i=0,l=f.length;if(el){function removeFromShadows(root){for(var el of root.querySelectorAll('*')){if(el.shadowRoot){el.shadowRoot.getElementById(id).remove();removeFromShadows(el.shadowRoot);}}}el.remove();if(l){for(i=0;i<l;i++){try{f[i].contentWindow.document.getElementById(id).remove();removeFromShadows(f[i].contentWindow.document);}catch(e){console.log(e)}}}removeFromShadows(d);}else{s=d.createElement('style');s.id=id;s.appendChild(d.createTextNode('*{line-height:1.5 !important;letter-spacing:${selectTextSpace.value}em !important;word-spacing:0.16em !important;}p{margin-bottom:2em !important;}'));function applyToShadows(root){for(var el of root.querySelectorAll('*')){if(el.shadowRoot){el.shadowRoot.appendChild(s.cloneNode(true));applyToShadows(el.shadowRoot);}}}d.getElementsByTagName('head')[0].appendChild(s);for(i=0;i<l;i++){try{f[i].contentWindow.document.getElementsByTagName('head')[0].appendChild(s.cloneNode(true));applyToShadows(f[i].contentWindow.document);}catch(e){console.log(e)}}applyToShadows(d);}})()`;
@@ -15,4 +16,10 @@ generate.addEventListener('click', (event) => {
     else {  result.textContent = `Please select any of the above fields to generate bookmarklet.`;}
 
 }); 
+generate.selectTextSpace.value
+function myFunction() {
 
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+  }
