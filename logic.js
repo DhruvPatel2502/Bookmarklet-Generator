238070af3a15c4365412890d7e48c103c108e540
DhruvPatel2502 <93792385+DhruvPatel2502@users.noreply.github.com>
2023-02-07 17:29:28 -0500

    made changes in copy notification

--- logic.js
+++ logic.js
@@ -32,7 +32,6 @@ function copyCode() {
   var copyText = document.getElementById("generatedCode");
 
   // Select the text field
-  copyText.select();
   copyText.setSelectionRange(0, 99999); // For mobile devices
 
   // Copy the text inside the text field
